f25cdb9bb4853e7557d231abac3dc6523a67ee1f
harryzhang1018 <79550723+harryzhang1018@users.noreply.github.com>
2022-02-16 11:14:31 -0600

    Add files via upload

---
+++ assignments/a03/bEuler-msd.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+step_size=10**(-4)
+duration = 5
+t_range = np.arange(0,duration+step_size,step_size)
+
+m = 1000
+ks = 50000
+cs = 4000
+los = 0.4
+g = 10
+
+x_ini = m*g/(4*ks)
+x_eq = los - x_ini
+v_ini = 0
+
+def bEuler(x,v,h):
+    v_next = (v-x *h* ks/m ) / (1+h* cs/m + ks*h**2/m)
+    x_next = x + h*v
+    return np.array([x_next,v_next])
+
+x = np.zeros((2,t_range.size))
+x[0,0] = x_ini
+x[1,0] = v_ini
+
+for i in np.arange(1,t_range.size):
+    x[0,i] = bEuler(x[0,i-1],x[1,i-1],step_size)[0]
+    x[1,i] = bEuler(x[0,i-1],x[1,i-1],step_size)[1]
+
+position = np.zeros(t_range.size)
+position = x[0,:] + x_eq
+
+plt.figure()
+plt.plot(t_range,position)
+plt.xlabel('Time (s)')
+plt.ylabel('Position (m)')
+plt.title('Position of Sprung Mass')
+plt.show()
+
+pos_solution = np.exp(-2*t_range) * ( 1/20 * np.cos(np.sqrt(46)* t_range)+ np.sin(np.sqrt(46)* t_range) / (10*np.sqrt(46)))+0.35
+vel_solution = -5 * np.exp(-2 * t_range) * np.sin(np.sqrt(46)*t_range) / (2*np.sqrt(46))
+
+e_pos4 = np.max(np.abs(pos_solution-position))
+e_vel4 = np.max(np.abs(x[1,:]-vel_solution))
+
+print('largest position error (h=1e-4) is : ',e_pos4)
+print('largest velocity error (h=1e-4) is : ',e_vel4)
+
+def calError(h):
+    tspan = np.arange(0,duration+h,h)
+    x_vec = np.zeros((2,tspan.size))
+    x_vec[0,0] = x_ini  
+    x_vec[1,0] = v_ini
+    for i in np.arange(1,tspan.size):
+        x_vec[0,i] = bEuler(x_vec[0,i-1],x_vec[1,i-1],h)[0]
+        x_vec[1,i] = bEuler(x_vec[0,i-1],x_vec[1,i-1],h)[1]
+    positionOfMass = np.zeros(tspan.size)
+    positionOfMass = x_vec[0,:] + x_eq
+    pos_sol = np.exp(-2*tspan) * ( 1/20 * np.cos(np.sqrt(46)* tspan)+ np.sin(np.sqrt(46)* tspan) / (10*np.sqrt(46)))+0.35
+    vel_sol = -5 * np.exp(-2 * tspan) * np.sin(np.sqrt(46)*tspan) / (2*np.sqrt(46))
+    err_pos = np.max(np.abs(pos_sol-positionOfMass))
+    err_vel = np.max(np.abs(x_vec[1,:]-vel_sol))
+    return np.array(([err_pos],[err_vel]))
+
+StepSizeList = np.array([10**(-1),10**(-2),10**(-3),10**(-4),10**(-5)])
+e_pos = np.zeros(5)
+e_vel = np.zeros(5)
+
+for i in np.arange(0,StepSizeList.size):
+    e_pos[i] = calError(StepSizeList[i])[0]
+    e_vel[i] = calError(StepSizeList[i])[1]
+
+plt.figure()
+plt.loglog(StepSizeList,e_pos,label='Position Error')
+plt.loglog(StepSizeList, e_vel, label='Velocity Error')
+plt.legend()
+plt.xlabel('Time Step (s)')
+plt.ylabel('Error (m or m/s)')
+plt.title('Error vs Time Step')
+plt.show()
